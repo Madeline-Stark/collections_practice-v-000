4ee5dc0082537fbffc78147afec9e530fb3b405c
Madeline Stark <madeline.stark1@gmail.com>
2018-03-27 01:54:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -67,9 +67,9 @@ end
 def add_s(array)
   array.each_with_index.collect do |string, index|
     if index == 1
-      string
+    "#{string}"
     else
-    string = "string#{"s"}"
+    "#{string}s"
   end
 end
 end
